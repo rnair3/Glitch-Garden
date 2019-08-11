406dd95c70fae50c3bb72d2fd9d510d955b500ab
Rinita Nair <rnair3@uncc.edu>
2019-08-11 17:58:18 -0400

    Basic Setup

---
+++ Glitch Garden/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+    [SerializeField] int waitForTime = 4;
+    int currentSceneIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if(currentSceneIndex == 0)
+        {
+            StartCoroutine(GoToStartScene());
+        }
+    }
+
+    IEnumerator GoToStartScene()
+    {
+        yield return new WaitForSeconds(waitForTime);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(++currentSceneIndex);
+    }
+}
