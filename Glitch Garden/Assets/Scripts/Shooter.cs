698be10dce44097c7d555f23b7dde72026b39f8e
Rinita Nair <rnair3@uncc.edu>
2019-08-18 16:59:58 -0400

    Scoring system and adding defenders

--- Glitch Garden/Assets/Scripts/Shooter.cs
+++ Glitch Garden/Assets/Scripts/Shooter.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,16 +7,54 @@ public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject bullet;
     [SerializeField] GameObject gun;
+
+    AttackerSpawner myLaneSpawner;
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetLaneSpawner();
+        animator = GetComponent<Animator>();
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach(AttackerSpawner spawner in spawners)
+        {
+            bool isCloseEnough = Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon;
+            if (isCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isAttackerInLane())
+        {
+            //change animation state to shooting
+            animator.SetBool("isAttacking", true);
+            
+        }
+        else
+        {
+            //start idle animation
+            animator.SetBool("isAttacking", false);
+        }
+    }
+
+    private bool isAttackerInLane()
+    {
+        if(myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        return true;
     }
 
     public void Shoot()
